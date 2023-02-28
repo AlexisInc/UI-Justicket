18410180c4830d50ed7a5632409d27c226ddede0
Hamrani Khaled <khaled.hamrani.etu@b12p7.fil.univ-lille.fr>
2023-02-28 17:20:22 +0100

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(

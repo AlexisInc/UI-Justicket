386ab8676ac239611f8a55c440bb14f21aae0be4
lucas.david.etu <lucas.david.etu@univ-lille.fr>
2023-03-07 17:48:34 +0100

    ajout bouton connexion/deconnexion

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,58 @@
-import * as React from 'react';
-import './App.css';
+import { NetworkType } from "@airgap/beacon-types";
+import { BeaconWallet } from "@taquito/beacon-wallet";
+import { TezosToolkit } from "@taquito/taquito";
+import { useEffect, useState } from "react";
+import "./App.css";
+import ConnectButton from "./ConnectWallet.tsx";
+import DisconnectButton from "./DisconnectWallet.tsx";
 
 function App() {
+  const [Tezos, setTezos] = useState<TezosToolkit>(
+      new TezosToolkit("https://ghostnet.tezos.marigold.dev")
+  );
+  const [wallet, setWallet] = useState<BeaconWallet>(
+      new BeaconWallet({
+        name: "Training",
+        preferredNetwork: NetworkType.GHOSTNET,
+      })
+  );
+
+  useEffect(() => {
+    Tezos.setWalletProvider(wallet);
+    (async () => {
+      const activeAccount = await wallet.client.getActiveAccount();
+      if (activeAccount) {
+        setUserAddress(activeAccount.address);
+        const balance = await Tezos.tz.getBalance(activeAccount.address);
+        setUserBalance(balance.toNumber());
+      }
+    })();
+  }, [wallet]);
+
+  const [userAddress, setUserAddress] = useState<string>("");
+  const [userBalance, setUserBalance] = useState<number>(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <ConnectButton
+              Tezos={Tezos}
+              setUserAddress={setUserAddress}
+              setUserBalance={setUserBalance}
+              wallet={wallet}
+          />
+
+          <DisconnectButton
+              wallet={wallet}
+              setUserAddress={setUserAddress}
+              setUserBalance={setUserBalance}
+          />
+
+          <div>
+            I am {userAddress} with {userBalance} mutez
+          </div>
+        </header>
+      </div>
   );
 }
 

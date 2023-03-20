7a11b8a74fb0e1594346f808dabd4d6acd1ea4d1
Zakaria OUAICHOUCHE <zakariaouaichouche@gmail.com>
2023-03-20 15:19:36 +0100

    Add Billets

---
+++ src/model/billet.tsx
@@ -0,0 +1,5 @@
+export type Billet =   {
+    name : string,
+    description: string,
+    id : string
+}
--- src/pages/billet.jsx
+++ src/pages/billet.jsx
@@ -1,8 +1,110 @@
 
-
+import { Button, Card } from 'react-bootstrap';
 const Billet = ()=>{
     return (
+        <>
         <h1>Mes billets</h1>
+        <section className="billets">
+       <Card className="billet">
+            <Card.Header as="h5">Nom de billet</Card.Header>
+            <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                    Id billet
+                </Card.Text>
+                <Button variant="primary" >Transférer</Button>
+                <Button variant="primary">Burn</Button>
+             </Card.Body>
+        </Card>
+
+        <Card className="billet">
+            <Card.Header as="h5">Nom de billet</Card.Header>
+            <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                    Id billet
+                </Card.Text>
+                <Button variant="primary">Transférer</Button>
+                <Button variant="primary">Burn</Button>
+            </Card.Body>
+        </Card>
+
+        <Card className="billet">
+            <Card.Header as="h5">Nom de billet</Card.Header>
+            <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                    Id billet
+                </Card.Text>
+                <Button variant="primary">Transférer</Button>
+                <Button variant="primary">Burn</Button>
+            </Card.Body>
+        </Card>
+
+        <Card className="billet">
+            <Card.Header as="h5">Nom de billet</Card.Header>
+            <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                    Id billet
+                </Card.Text>
+                <Button variant="primary">Transférer</Button>
+                <Button variant="primary">Burn</Button>
+            </Card.Body>
+        </Card>
+
+        <Card className="billet">
+            <Card.Header as="h5">Nom de billet</Card.Header>
+            <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                    Id billet
+                </Card.Text>
+                <Button variant="primary">Transférer</Button>
+                <Button variant="primary">Burn</Button>
+            </Card.Body>
+        </Card>
+
+        <Card className="billet">
+            <Card.Header as="h5">Nom de billet</Card.Header>
+            <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                    Id billet
+                </Card.Text>
+                <Button variant="primary">Transférer</Button>
+                <Button variant="primary">Burn</Button>
+            </Card.Body>
+        </Card>
+
+        <Card className="concert">
+            <Card.Header as="h5">Nom de billet</Card.Header>
+            <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                    Id billet
+                </Card.Text>
+                <Button variant="primary">Transférer</Button>
+                <Button variant="primary">Burn</Button>
+            </Card.Body>
+        </Card>
+
+                <Card className="concert">
+            <Card.Header as="h5">Nom de billet</Card.Header>
+            <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                    Id billet
+                </Card.Text>
+                <Button variant="primary">Transférer</Button>
+                <Button variant="primary">Burn</Button>
+            </Card.Body>
+        </Card>
+
+        
+
+        </section>
+      </>
         
     );
 }
